9c11015d7d003157c9ad47540da324ecbe704c96
Kohsuke Sakaguchi <sanbonkihara@gmail.com>
2021-03-07 00:06:06 +0900

    solve ABC113-B

---
+++ similar_problems/q3/abc113_b.py
@@ -0,0 +1,14 @@
+#https://atcoder.jp/contests/abc113/tasks/abc113_b
+
+def temp(T) -> float:
+    "平均気温の関数"
+    return lambda x: T - x * 0.006
+
+N = int(input())
+T, A = [i for i in map(int, input().split())]
+H = [i for i in map(int, input().split())]
+
+
+H_temp = [h for h in map(temp(T), H)]
+fav_temp = [abs(h-A) for h in H_temp]
+print(fav_temp.index(min(fav_temp))+1)
